fef6b3c69e2b272bdba9a04e5ded36716d40a8b6
Adrian <hannsadrian@gmail.com>
2020-04-26 16:23:46 +0200

    Add chapter entries

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,13 +1,70 @@
 <script>
     import {Link} from "svelte-routing";
+
+    const entries = [
+      {
+        name: "🕰 Geschichte",
+        link: "/history",
+        description: 'Ursprünglich sollte HTML ausschließlich für den Inhalt genutzt werden.<br/>Befehle wie <span class="font-mono">&#60;b&#62;</span> oder <span class="font-mono">&#60;i&#62;</span> waren nie geplant.'
+      },
+      {
+        name: "🦐 Einbinden in HTML",
+        link: "/embed",
+        description: 'Damit CSS Attribute, Klassen und Media Queries angewendet werden,<br/>muss das CSS Stylesheet eingebunden werden.'
+      },
+      {
+        name: "🏜 CSS Attribute",
+        link: "/attributes",
+        description: 'Für die Manipulation von bestimmt Werten des Aussehens<br/>verwendet man bestimmte CSS Attribute.'
+      },
+      {
+        name: "🧩 Selektoren",
+        link: "/selectors",
+        description: 'Es gibt verschiedene Möglichkeiten CSS Klassen anzuwenden.<br/>Dazu verändert man die Beschriftung der Klasse mit bestimmten Symbolen.'
+      },
+      {
+        name: "🍻 Kombinatoren",
+        link: "/combinators",
+        description: 'Durch Kombinatoren ist es möglich, Selektoren miteinander zu kombinieren.<br/>Dadurch werden Attribute nur unter bestimmten Umständen angewendet.'
+      },
+      {
+        name: "🍟 Pseudoklassen",
+        link: "/pseudoclasses",
+        description: 'Pseudoklassen sind wie Kombinatoren dazu da,<br/>Attribute nur unter bestimmten Bedingungen anzuwenden.'
+      },
+      {
+        name: "🔍 Media Queries",
+        link: "/mediaqueries",
+        description: 'Mit Media Queries ist es möglich Geräte-Vorraussetzungen abzufragen,<br/>dadurch können Inhalte auf Handys anders dargestellt werden.'
+      },
+      {
+        name: "🎞 Frameworks",
+        link: "/frameworks",
+        description: 'Frameworks erleichtern die Arbeit mit CSS,<br/>sie enthalten vordefiniertes Aussehen für bestimmte Elemente'
+      },
+    ]
 </script>
 
-<main class="mt-12 pb-10 md:mt-20 mx-20">
+<main class="pt-12 h-full pb-10 md:pt-32 mx-20">
   <h1 class="-ml-3 mb-4 text-5xl font-dosis font-bold flex"><img class="h-16 my-auto" alt="" src="/logo.svg" /><span class="my-auto pt-1">CSS</span></h1>
   <p class="font-semibold text-sm text-gray-600">HANNS ADRIAN BÖHME</p>
   <p>
-    Praktischer Teil zu dem zugehörigen <Link to="download"><span class="underline cursor-pointer text-blue-600 hover:text-blue-700">Referat</span></Link><br/>
+    Praktischer Teil zum zugehörigen <Link to="download"><span class="hover:underline cursor-pointer text-blue hover:text-blue-700">Referat</span></Link><br/>
     über <b>C</b>ascading <b>S</b>tyle <b>S</b>heets.
   </p>
-  <hr class="my-6 border"/>
+  <hr class="mt-4 mb-3 border"/>
+  <div class="xl:flex xl:flex-wrap">
+  {#each entries as entry}
+    <div class="xl:w-1/2">
+      <Link to={entry.link}>
+        <div class="m-2 w-auto p-4 cursor-pointer hover:bg-white rounded border border-gray-400 transition duration-200 shadow hover:shadow-md">
+          <h1 class="text-xl -mt-1 font-bold">{entry.name}</h1>
+          <p class="text-gray-700 text-sm">
+            {@html entry.description}
+          </p>
+        </div>
+      </Link>
+    </div>
+    {/each}
+  </div>
 </main>
