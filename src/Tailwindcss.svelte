671ffd04143fcce456b0b390ace0e569e0815c3c
Adrian <hannsadrian@gmail.com>
2020-05-02 21:21:14 +0200

    Target main elements instead of html

--- src/Tailwindcss.svelte
+++ src/Tailwindcss.svelte
@@ -2,9 +2,9 @@
 @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@400;700&display=swap');
 @import url('https://rsms.me/inter/inter.css');
 
-html { font-family: 'Inter',  system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; }
+main { font-family: 'Inter',  system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; }
 @supports (font-variation-settings: normal) {
-  html { font-family: 'Inter var', system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; }
+  main { font-family: 'Inter var', system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; }
 }
 
 ::selection {
