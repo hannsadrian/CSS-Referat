4e5d594dd439cf54a34b6d3b5210ebc43cf9d2a6
Adrian <hannsadrian@gmail.com>
2020-05-02 21:20:55 +0200

    Add proper formatting with customization

--- src/utils/Layout/CodeContainer.svelte
+++ src/utils/Layout/CodeContainer.svelte
@@ -1,5 +1,15 @@
-<div class="rounded border-2 px-4 py-3 border-gray-500 bg-gray-200">
-    <pre style="border-width: 0 !important;" class="prettyprint">
+<script>
+    import { onMount } from 'svelte';
+    export let format = "";
+    export let shadow = false;
+
+    onMount(async () => {
+        PR.prettyPrint()
+    });
+</script>
+
+<div class={"rounded border-2 px-4 py-3 border-gray-500 bg-gray-200" + (!!shadow ? " shadow-lg" : "")}>
+    <pre style="border-width: 0 !important;" class={"prettyprint" + (format !== "" ? " lang-"+format : "")}>
         <slot/>
     </pre>
 </div>
