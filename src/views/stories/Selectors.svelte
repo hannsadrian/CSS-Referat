0c23f97bae4255f909eabbf6770005c622853683
Adrian <hannsadrian@gmail.com>
2020-05-03 14:06:14 +0200

    Add class, id, attribute and element selectors

--- src/views/stories/Selectors.svelte
+++ src/views/stories/Selectors.svelte
@@ -27,3 +27,21 @@
     </CodeContainer>
     <Annotation>Auf dieser Unterseite geht es um Selektoren wie <code>.text</code> und wie man sie Verwenden kann</Annotation>
 
+    <hr class="mt-4 mb-3 border"/>
+
+    <Subtitle>Klassen Selektoren</Subtitle>
+    <DualCodeContainer html={"<p class=\"text\">Klassen Selektoren</p>"} css={".text { color: blue; }"} />
+    <Annotation>Klassen Selektoren werden auf Elemente mit dem Attribut class angewendet. Es ist möglich mehrere Klassen auf ein Element anzuwenden.</Annotation>
+
+    <Subtitle>ID Selektoren</Subtitle>
+    <DualCodeContainer html={"<p id=\"text\">ID Selektoren</p>"} css={"#text { color: blue; }"} />
+    <Annotation>ID Selektoren werden auf Elemente mit dem Attribut id angewendet. Ein Element kann nur eine ID haben.</Annotation>
+
+    <Subtitle>Attribut Selektoren</Subtitle>
+    <DualCodeContainer html={"<p text>Attribut Selektoren</p>"} css={"[text] { color: blue; }"} />
+    <Annotation>Attribut Selektoren werden auf Elemente mit dem entsprechend Attribut angewendet. Ein Element kann mehrere Attribute haben.</Annotation>
+
+    <Subtitle>Element Selektoren</Subtitle>
+    <DualCodeContainer html={"<p>Element Selektoren</p>"} css={"p { color: blue; }"} />
+    <Annotation>Element Selektoren werden immer direkt automatisch auf Elemente mit dem Tag, den sie definieren, angewendet.</Annotation>
+</Container>
