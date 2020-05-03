c29f26b8e5e62a383771042a276e28b5207e39ee
Adrian <hannsadrian@gmail.com>
2020-05-03 14:04:54 +0200

    Add Component for two code containers in a row

---
+++ src/utils/Layout/DualCodeContainer.svelte
@@ -0,0 +1,14 @@
+<script>
+    import CodeContainer from "./CodeContainer.svelte";
+    export let html;
+    export let css;
+</script>
+
+<div class="flex">
+    <div class="w-1/2 pr-1">
+        <CodeContainer format="html">{html}</CodeContainer>
+    </div>
+    <div class="w-1/2 pl-1">
+        <CodeContainer format="css">{css}</CodeContainer>
+    </div>
+</div>
