022bbba338266fecc4c5c8373048b12571964abb
Adrian <hannsadrian@gmail.com>
2020-05-03 14:04:30 +0200

    Add styled Link component

---
+++ src/utils/Layout/Hyperlink.svelte
@@ -0,0 +1,7 @@
+<script>
+    import {Link} from "svelte-routing";
+
+    export let to;
+</script>
+
+<Link {to}><span class="hover:underline cursor-pointer text-blue hover:text-blue-700"><slot/></span></Link>
