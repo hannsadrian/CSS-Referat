66d46d308d2b79ec5ce82577a05349d769589231
Adrian <hannsadrian@gmail.com>
2020-05-03 15:52:02 +0200

    Make position swapable

--- src/utils/Layout/DualCodeContainer.svelte
+++ src/utils/Layout/DualCodeContainer.svelte
@@ -1,14 +1,23 @@
 <script>
     import CodeContainer from "./CodeContainer.svelte";
+
     export let html;
     export let css;
+    export let cssFirst = false;
 </script>
 
 <div class="flex">
-    <div class="w-1/2 pr-1">
+    {#if cssFirst}
+        <div class="w-1/2 pr-2">
+            <CodeContainer format="css">{css}</CodeContainer>
+        </div>
+    {/if}
+    <div class="w-1/2">
         <CodeContainer format="html">{html}</CodeContainer>
     </div>
-    <div class="w-1/2 pl-1">
-        <CodeContainer format="css">{css}</CodeContainer>
-    </div>
+    {#if !cssFirst}
+        <div class="w-1/2 pl-2">
+            <CodeContainer format="css">{css}</CodeContainer>
+        </div>
+    {/if}
 </div>
